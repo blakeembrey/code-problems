55ae828ffe33edd5a5240fe9b6903ed54df5acf9
Blake Embrey <hello@blakeembrey.com>
2013-05-22 16:56:03 -0700

    Refactor anagram solution.

--- anagram-detection/index.js
+++ anagram-detection/index.js
@@ -2,22 +2,68 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 var data = '',
-    findAnagrams;
+    hashString;
 
-findAnagrams = function (string) {
-  var anagrams = {};
+hashString = function (str) {
+  // Map characters to prime numbers to multiply
+  var charMap = {
+    'a': 2,
+    'b': 3,
+    'c': 5,
+    'd': 7,
+    'e': 11,
+    'f': 13,
+    'g': 17,
+    'h': 19,
+    'i': 23,
+    'j': 29,
+    'k': 31,
+    'l': 37,
+    'm': 41,
+    'n': 43,
+    'o': 47,
+    'p': 53,
+    'q': 59,
+    'r': 61,
+    's': 67,
+    't': 71,
+    'u': 73,
+    'v': 79,
+    'w': 83,
+    'x': 89,
+    'y': 97,
+    'z': 101,
+    'A': 103,
+    'B': 107,
+    'C': 109,
+    'D': 113,
+    'E': 127,
+    'F': 131,
+    'G': 137,
+    'H': 139,
+    'I': 149,
+    'J': 151,
+    'K': 163,
+    'L': 167,
+    'M': 173,
+    'N': 179,
+    'O': 181,
+    'P': 191,
+    'Q': 193,
+    'R': 197,
+    'S': 199,
+    'T': 211,
+    'U': 223,
+    'V': 227,
+    'W': 229,
+    'X': 233,
+    'Y': 239,
+    'Z': 241
+  };
 
-  (function findAnagrams (str, remaining) {
-    if (!remaining) {
-      return anagrams[str] = true;
-    }
-
-    for (var i = 0; i < remaining.length; i++) {
-      findAnagrams(str + remaining[i], remaining.substr(0, i) + remaining.substr(i + 1));
-    }
-  })('', string);
-
-  return anagrams;
+  return str.split('').reduce(function (memo, char) {
+    return memo * charMap[char];
+  }, 1);
 };
 
 process.stdin.on('data', function (chunk) {
@@ -25,14 +71,14 @@ process.stdin.on('data', function (chunk) {
 });
 
 process.stdin.on('end', function () {
-  var input    = data.split('\n'),
-      string   = input[0],
-      length   = input[1].length,
-      anagrams = findAnagrams(input[1]),
-      total    = 0;
+  var input   = data.split('\n'),
+      string  = input[0],
+      length  = input[1].length,
+      anagram = hashString(input[1]),
+      total   = 0;
 
   for (var i = 0; i < (string.length - length); i++) {
-    if (anagrams[string.substr(i, length)]) {
+    if (hashString(string.substr(i, length)) === anagram) {
       total += 1;
     }
   }
