915baf47c560381861c0e56537a007098b44d6ae
Almewty <thomasbohn@online.de>
2013-06-12 11:19:36 +0200

    0 AND 1 are no prime numbers
    http://en.wikipedia.org/wiki/Prime_number

--- prime-number/index.js
+++ prime-number/index.js
@@ -1,5 +1,5 @@
 var primeNumber = function (n) {
-  if (n < 1) { return false; }
+  if (n <= 1) { return false; }
 
   for (var i = 2, l = Math.floor(Math.pow(n, 0.5)); i <= l; i++) {
     if (n % i === 0) {
