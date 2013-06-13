e2fefcacff9ed5880cb4c48503fef6e6a9cfde7f
Blake Embrey <hello@blakeembrey.com>
2013-06-13 12:31:02 +1000

    Implement fix for the issue mentioned in #13.

--- array-pair-sum/index.js
+++ array-pair-sum/index.js
@@ -7,11 +7,11 @@ var arrayPairSum = function (k, array) {
   // time complexity of O(n) - the naive solution consists of two for loops
   // which results in a complexity of O(n^2)
   array.forEach(function (number) {
-    hash[number] = hash[number] + 1 || 1;
-
     for (var i = 0; i < (hash[k - number] || 0); i++) {
       pairs.push([number, k - number]);
     }
+
+    hash[number] = hash[number] + 1 || 1;
   });
 
   return pairs;
