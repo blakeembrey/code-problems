e4220535870e803958d3915a9713eed3d90bfc74
Gianni Gambetti <jex.pwns@gmail.com>
2013-06-14 16:03:23 -0400

    Optimization of Sum-Of-Array-Plus-One
    Removed single addition operation without array.reduce

--- sum-of-array-plus-one/sum-of-array-plus-one.js
+++ sum-of-array-plus-one/sum-of-array-plus-one.js
@@ -7,11 +7,11 @@ var plusOneSum = function (array) {
 
 // Without array.reduce method isn't much different
 var plusOneSum = function (array) {
-  var result = 0;
+  var result = array.length;
 
   for (var i = 0; i < array.length; i++) {
     result += array[i];
   }
 
-  return result + array.length;
+  return result;
 };
