1267b63d4e490c8d34402849dc9b2ae96c952942
Blake Embrey <hello@blakeembrey.com>
2013-06-16 09:16:31 +1000

    Clean up fibonacci solution and merge iterative solution.

--- fibonacci/fibonnaci.js
+++ fibonacci/fibonnaci.js
@@ -1,13 +1,19 @@
-var memoise = function (fn) {
-  var cache = {};
-  // Simple one parameter memoise since it's enough for the fibonacci function
-  return function (arg) {
-    if (arg in cache) { return cache[arg]; }
-
-    return cache[arg] = fn(arg);
-  };
+// Implementing recursive solution
+var fibonacci = function (n) {
+  if (n in fibonacci) { return fibonacci[n]; }
+  // Store the fibonacci values on the function itself
+  return fibonacci[n] = (n < 2) ? n : fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-var fibonacci = memoise(function (n) {
-  return (n < 2) ? n : fibonacci(n - 1) + fibonacci(n - 2);
-});
+// Implementing iterative solution
+var fibonnaci = function (n) {
+  var results = [0, 1];
+
+  if (n > 2) {
+    for (var i = 2; i < n; i++) {
+      results[i] = results[i - 2] + results[i - 1];
+    }
+  }
+
+  return results[n - 1];
+};
