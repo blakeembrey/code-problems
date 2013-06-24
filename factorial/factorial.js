1bb038d7f091d25ec1cd50d9da326dd75d1827f5
nootanghimire <nootan.ghimire@gmail.com>
2013-06-24 21:54:42 +0545

    Create factorial.js

---
+++ factorial/factorial.js
@@ -0,0 +1,25 @@
+
+// Implementing recursive solution
+module.exports = function factorial (number) {
+  if(number<=1) return 1;
+  return number*(factorial(number);
+}
+
+// Implementing iterative solution
+module.exports = function (number){
+   var result = 1 ;
+   for(i=1; i<=number; i++){
+     result *= i;
+   }
+   return result;
+}
+
+//Implementing iterative solution 2 (going backwards in loop)
+
+module.exports = function (number){
+   var result = 1 ;
+   for(i=number; i>1; i++){
+     result *= i;
+   }
+   return result;
+}
