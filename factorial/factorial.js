bc1b0573ba456bdd6f4238af3a06129f567fe474
nootanghimire <nootan.ghimire@gmail.com>
2013-06-25 13:07:33 +0545

    Added the parenthesis i missed!

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,7 +1,7 @@
 // Implementing recursive solution
 module.exports = function factorial (number) {
   if(number<=1) return 1;
-  return number*(factorial(number - 1);
+  return number*(factorial(number - 1));
 }
 
 // Implementing iterative solution
