bcf3c0f7da8233d6657ab65156d5bed8fb322292
Blake Embrey <hello@blakeembrey.com>
2013-06-25 13:15:38 +1000

    Rewrite the next highest number algorithm, all working now.

--- next-highest-number/next-highest-number.js
+++ next-highest-number/next-highest-number.js
@@ -1,36 +1,33 @@
 module.exports = function (number) {
-    var numberString = ('' + number),
-        length = numberString.length - 1,
-        l = length,
-        nextHighest;
+  var numberString = ('' + number),
+      length       = numberString.length - 1,
+      string       = '',
+      sorter;
 
-    nextHighest = function (string, value) {
-        string = string.split('').sort();
-        for (var i = 0; i < string.length; ++i) {
-            if (string[i] > value) {
-                var result = string[i];
-                string.splice(i, 1);
-                // return the next highest digist and the sorted string
-                // without this digist
-                return {
-                    value: result,
-                    rest: string.join("")
-                };
-            }
-        }
-    };
+  // Loop in reverse comparing all the digits to the one beside it
+  while (length--) {
+    // Compare the numbers beside each other
+    if (numberString.charAt(length) < numberString.charAt(length + 1)) {
+      // Start the string using the numbers up until the pivot point
+      string = numberString.substr(0, length);
+      // Sort all the numbers after the pivot
+      sorter = numberString.substr(length).split('').sort();
 
-    // Loop from the current right index to the left index
-    while (l--) {
-        // Once the character on the left is smaller than the current right
-        // position this will be our swap index
-        if (numberString.charAt(l) < numberString.charAt(l + 1)) {
-            // Find next highest digist on the right side of the
-            // swap index
-            var nextHighest = nextHighest(numberString.substr(l, length), numberString.charAt(l));
-            return numberString.substr(0, l) + nextHighest.value + nextHighest.rest;
+      // Loop through all the numbers and if the next largest number than the pivot
+      for (var i = 0; i < sorter.length; i++) {
+        if (sorter[i] > numberString.charAt(length)) {
+          // Splice the number from it's position and append it to the current string
+          string += sorter.splice(i, 1)[0];
+          break;
         }
+      }
+
+      // Append the rest of the numbers already in ascending order
+      string += sorter.join('');
+      // Return the string typecast back to a number
+      return +string;
     }
+  }
 
-    return number;
+  return number;
 };
