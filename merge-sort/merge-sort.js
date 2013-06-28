cbdc557af96c817f87dfba603021427ff91c05a0
Blake Embrey <hello@blakeembrey.com>
2013-06-28 15:57:13 +1000

    Clean up merge sort and add comparator function.

--- merge-sort/merge-sort.js
+++ merge-sort/merge-sort.js
@@ -1,39 +1,37 @@
-// MergeSort
-// Sorts the array by breaking it down into smaller chunks.
+// Sort the array by breaking it down into smaller chunks
+module.exports = function mergeSort (array, compare) {
+  // If's not an array or an array of just one element, it's already sorted
+  if (!Array.isArray(array) || array.length < 2) { return array; }
 
-module.exports = function(array) {
-  // Not an array, empty or array of 1 is already sorted
-	if(!Array.isArray(array) || array.length < 2)
-		return array;
+  var length = array.length,
+      middle = Math.floor(length * 0.5),
+      left   = array.slice(0, middle),
+      right  = array.slice(middle, length);
 
-	function merge(left, right) {
-		var result = [];
-		while(left.length || right.length) {
-			if(left.length && right.length) {
-				if(left[0] < right[0]) {
-					result.push(left.shift());
-				} else {
-					result.push(right.shift());
-				}
-			} else if (left.length) {
-				result.push(left.shift());
-			} else {
-				result.push(right.shift());
-			}
-		}
-		return result;
-	}
+  // Create a compare func if not passed in
+  if (typeof compare !== 'function') {
+    compare = function (a, b) {
+      return a > b ? 1 : -1;
+    };
+  }
 
-	function MergeSort(array){
-		var length = array.length,
-		mid = Math.floor(length * 0.5),
-		left = array.slice(0, mid),
-		right = array.slice(mid, length);
-		if(length === 1) {
-			return array;
-		}
-		return merge(MergeSort(left), MergeSort(right));
-	}
+  var merge = function (left, right) {
+    var result = [];
+    while (left.length || right.length) {
+      if (left.length && right.length) {
+        if (compare(left[0], right[0]) < 1) {
+          result.push(left.shift());
+        } else {
+          result.push(right.shift());
+        }
+      } else if (left.length) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+    return result;
+  }
 
-	return MergeSort(array);
+  return merge(mergeSort(left), mergeSort(right));
 }
