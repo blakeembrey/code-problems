1b390ef83c0ac6609587217f80679105081095a3
Blake Embrey <hello@blakeembrey.com>
2013-06-29 10:53:04 +1000

    Clean up solution.

--- prime-number/prime-number.js
+++ prime-number/prime-number.js
@@ -1,15 +1,13 @@
 module.exports = function (n) {
-  if (n < 2) { return false; }
-  
-  if (n == 2) {
-    return true;
-  }
-  
-  if (n % 2 == 0){
-    return false;
-  }
+  // Two is a prime number
+  if (n === 2) { return true; }
 
-  for (var i = 3, l = Math.floor(Math.pow(n, 0.5)); i <= l; i+=2) {
+  // Every other even number is not
+  if (n < 2 || !(n&1)) { return false; }
+
+  // Create a loop that will check every odd number up until n^1/2
+  for (var i = 3, l = Math.floor(Math.pow(n, 0.5)); i <= l; i += 2) {
+    // Evenly divisible
     if (n % i === 0) {
       return false;
     }
