293aa6cf38297b0a74a09ef5ca2a40c965b1bba5
YoussefKababe <youssef.kbe@gmail.com>
2013-07-03 18:31:28 +0100

    Add solution in Ruby

---
+++ array-pair-sum/array-pair-sum.rb
@@ -0,0 +1,11 @@
+def arrayPairSum number, array
+
+  pairs = []
+  
+  array.select do |a|
+      pairs.push([a, a]) if number - a == a && array.count(a) > 1 && !pairs.include?([a, a]) 
+      pairs.push([a, number - a]) if number - a != a && array.include?(number - a) && !pairs.include?([number - a, a])
+  end
+  
+  pairs
+end
