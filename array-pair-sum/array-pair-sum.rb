384b91793d2f1ba274376be9b9d3ff86f51d85b5
Youssef Kababe <hello@youssefkababe.com>
2013-07-03 18:40:49 +0100

    Update array-pair-sum.rb

--- array-pair-sum/array-pair-sum.rb
+++ array-pair-sum/array-pair-sum.rb
@@ -2,7 +2,7 @@ def arrayPairSum number, array
 
   pairs = []
   
-  array.select do |a|
+  array.each do |a|
       pairs.push([a, a]) if number - a == a && array.count(a) > 1 && !pairs.include?([a, a]) 
       pairs.push([a, number - a]) if number - a != a && array.include?(number - a) && !pairs.include?([number - a, a])
   end
