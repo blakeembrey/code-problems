29e0dafb44841a5f0bfd9c2f19544d445da4a817
Thomas Lamb <tclamb@ufl.edu>
2013-07-04 22:33:43 -0400

    word-positions test now calls the solution correctly

--- test/word-positions.js
+++ test/word-positions.js
@@ -3,12 +3,12 @@ var wordPositions = require('../word-positions/word-positions'),
 
 describe('word positions', function () {
   it('should return an array of word positions', function () {
-    assert.deepEqual(wordPositions('test')('test'), [0]);
-    assert.deepEqual(wordPositions('test test test')('test'), [0, 4, 9]);
-    assert.deepEqual(wordPositions('find a word in some sentence')('in'), [11]);
+    assert.deepEqual(wordPositions('test', 'test'), [0]);
+    assert.deepEqual(wordPositions('test test test', 'test'), [0, 4, 9]);
+    assert.deepEqual(wordPositions('find a word in some sentence', 'in'), [11]);
   });
 
   it('should return an empty array when not found', function () {
-    assert.deepEqual(wordPositions('it does not exist')('test'), []);
+    assert.deepEqual(wordPositions('it does not exist', 'test'), []);
   });
 });
