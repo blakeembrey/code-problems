7fb09c77aa16f26dacb73b15e116e6daa1781083
Thomas Lamb <tclamb@ufl.edu>
2013-07-04 22:18:45 -0400

    Queue problem solved in C++11

---
+++ queue/queue.hpp
@@ -0,0 +1,72 @@
+#ifndef STACK_HPP_INCLUDED
+#define STACK_HPP_INCLUDED
+
+#include <utility> // std::forward
+#include <memory>  // std::move
+
+template<typename T>
+class Queue {
+public:
+     Queue() : head{nullptr}, tail{nullptr} {};
+    ~Queue();
+
+    template<typename U>
+    void enqueue(U&& t);
+       T dequeue();
+
+    inline bool empty() const { return head == nullptr; };
+
+private:
+    struct Node {
+            T data;
+        Node* next;
+
+        template<typename U>
+        Node(U&& d, Node* n) : data{std::forward<U>(d)}, next{n} {};
+    };
+
+    Node* head;
+    Node* tail;
+};
+
+template<typename T>
+Queue<T>::~Queue() {
+    // Rewriting functionality of dequeue here avoids a call to the move
+    // constructor for each element in the stack. For code golf, use:
+    //    while(!empty()) dequeue();
+    while(!empty()) {
+        auto old_head = head;
+                 head = head->next;
+        delete old_head;
+    }
+}
+
+template<typename T>
+template<typename U>
+void Queue<T>::enqueue(U&& t) {
+    auto node = new Node{std::forward<U>(t), nullptr};
+    if(empty())
+        head = node;
+    else
+        tail->next = node;
+    tail = node;
+}
+
+template<typename T>
+T Queue<T>::dequeue() {
+    if(empty())
+        throw;
+
+    auto old_head = head;
+    auto old_data = std::move(head->data);
+             head = head->next;
+
+    // actually unnecessary
+    /* if(empty()) */
+    /*     tail = nullptr; */
+
+    delete old_head;
+    return old_data;
+}
+
+#endif // STACK_HPP_INCLUDED
