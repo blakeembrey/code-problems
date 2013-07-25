b41a91ff7791134367d30024d315a1a71c0038c7
Taylor Shuler <taylorshuler@aol.com>
2013-07-25 14:13:33 -0400

    Compacted Ruby bubble-sort algorithm

--- bubble-sort/bubble-sort.rb
+++ bubble-sort/bubble-sort.rb
@@ -1,11 +1,9 @@
-def bubble_sort array
+def bubble_sort(array)
   i = 0
-  while i < array.size-1
-    j = i+1
+  while i < array.size - 1
+    j = i + 1
     while j < array.size
-      if array[i] > array[j]
-        array[i] , array[j] = array[j] , array[i]
-      end
+      array[i], array[j] = array[j], array[i] if array[i] > array[j]
       j = j + 1
     end
     i = i + 1
