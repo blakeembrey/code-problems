910fd0610bd020db7bc988f3f694f9c981a94260
Tim Sze <timmysze@gmail.com>
2013-07-24 22:31:44 -0700

    Add O(logn) matrix solution to fibonacci.

--- fibonacci/fibonnaci.js
+++ fibonacci/fibonnaci.js
@@ -6,7 +6,7 @@ var fibonacci = function (n) {
 };
 
 // Implementing iterative solution
-var fibonnaci = function (n) {
+var fibonacci = function (n) {
   var results = [0, 1];
 
   if (n > 2) {
@@ -16,4 +16,54 @@ var fibonnaci = function (n) {
   }
 
   return results[n - 1];
+};
+
+// Implementing O(logn) matrix solution
+var fibonacci = function (n) {
+  var memo = [0, [[0, 1], [1, 1]]];
+
+  var matrixMultiply = function (A, B) {
+    var C;
+
+    if (Array.isArray(B[0])) {
+      C = [[], []];
+      C[0][0] = A[0][0] * B[0][0] + A[1][0] * B[0][1];
+      C[0][1] = A[0][1] * B[0][0] + A[1][1] * B[0][1];
+      C[1][0] = A[0][0] * B[1][0] + A[1][0] * B[1][1];
+      C[1][1] = A[0][1] * B[1][0] + A[1][1] * B[1][1];
+    } else {
+      C = [];
+      C[0] = A[0][0] * B[0] + A[1][0] * B[1];
+      C[1] = A[0][1] * B[0] + A[1][1] * B[1];
+    }
+
+    return C;
+  };
+
+  // Calculates fibonacci spiral transformation matrix
+  var calcFibSpiral = function (n) {
+    var count = 1;
+    var T;
+
+    if (n & 1) {
+      T = [[0, 1], [1, 1]];
+      n -= 1;
+    } else {
+      T = [[1, 0], [0,1 ]];
+    }
+
+    while (n > 0) {
+      count++;
+      if (!memo[count]) {
+        memo[count] = matrixMultiply(memo[count - 1], memo[count - 1]);
+      }
+      if ((n >>= 1) & 1) {
+        T = matrixMultiply(T, memo[count]);
+      }
+    }
+
+    return T;
+  };
+
+  return matrixMultiply(calcFibSpiral(n - 2), [1, 1])[1];
 };
