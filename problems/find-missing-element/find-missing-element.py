efb4f75911edc8847026d1ae29d0f909f79240a9
acrich <me@shayacrich.com>
2013-11-17 11:24:12 +0200

    fixed spacing

--- problems/find-missing-element/find-missing-element.py
+++ problems/find-missing-element/find-missing-element.py
@@ -2,10 +2,12 @@
 
 def difference_set(orig, shuffled):
     """finds the missing element using a set."""
+
     return set(orig).difference(set(shuffled)).pop()
 
 def difference_iterative(orig, shuffled):
     """finds the missing element by iterating over the list"""
+
     for x in orig:
         if not x in shuffled:
             return x
