59a076f4fb6ea4dfc39f6033bc4adb7d3771ab7e
Youssef Kababe <youssef.kbe@gmail.com>
2013-12-23 01:23:58 +0000

    Anagram Detection solution in Ruby, using RegEx

---
+++ solutions/ruby/anagram-detection-regex.rb
@@ -0,0 +1,8 @@
+def anagram string, child
+  count = 0
+  reg = /\A#{ "[#{ child }]{#{ child.length }}"}\z/
+  for i in 0...string.length
+    count += 1 if string[i, child.length] =~ reg
+  end
+  count
+end
