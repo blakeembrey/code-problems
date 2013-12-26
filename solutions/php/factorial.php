81714d1881ec72729d8c6c8f345a7798350353bb
Blake Embrey <hello@blakeembrey.com>
2013-12-25 23:19:17 -0500

    Clean up PHP factorial solution.

--- solutions/php/factorial.php
+++ solutions/php/factorial.php
@@ -1,32 +1,35 @@
 <?php
 
-    // Iterative solution
-    function factorial($number){
-        $result = 1;
-        for($i=1; $i <= $number ; $i++){
-            $result *=$i;
-        }
-        return $result;
+  // Iterative solution.
+  function factorial ($number) {
+    $result = 1;
+
+    for ($i = 1; $i <= $number; $i++) {
+      $result *= $i;
     }
 
-    // Iterative using a reverse loop
-    function factorialReverse($number){
-        $result = 1;
-        while($number !=1){
-            $result *= $number;
-            $number--;
-        }
-        return $result;
+    return $result;
+  }
+
+  // Iterative using a reverse loop.
+  function factorialReverse ($number) {
+    $result = 1;
+
+    while ($number != 1) {
+      $result *= $number;
+      $number--;
     }
 
-    // Recursive solution
-    function factorialRecursive($number){
-        if($number ==1){
-            return 1;
-        }
-        else{
-            return $number *factorialRecursive($number-1);
-        }
+    return $result;
+  }
+
+  // Recursive solution.
+  function factorialRecursive ($number) {
+    if ($number == 1) {
+      return 1;
     }
+
+    return $number * factorialRecursive($number - 1);
+  }
 
 ?>
