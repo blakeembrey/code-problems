34c6837b7b5e6e0021ea7579c99d0e3e9cff87fd
Nurazem Kaldybaev <nurazemkg@gmail.com>
2014-01-22 21:40:40 +0400

    Modify styling to a proper one

--- solutions/python/array-pair-sum.py
+++ solutions/python/array-pair-sum.py
@@ -1,6 +1,5 @@
 """solution to the array pair sum problem"""
 
-
 def pair_sum_arrays(k, arr):
     """returns the array of pairs using an iterative method."""
 
@@ -10,4 +9,5 @@ def pair_sum_arrays(k, arr):
         for j in range(i + 1, len(arr)):
             if arr[i] + arr[j] == k:
                 result.append([arr[i], arr[j]])
+
     return result
