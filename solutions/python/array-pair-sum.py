49a578153696413c0cfbd80a0def679bc15a5168
Nurazem Kaldybaev <nurazemkg@gmail.com>
2014-01-22 19:50:13 +0400

    Add Python solution to Array Pair Sum problem

---
+++ solutions/python/array-pair-sum.py
@@ -0,0 +1,13 @@
+"""solution to the array pair sum problem"""
+
+
+def pair_sum_arrays(k, arr):
+    """returns the array of pairs using an iterative method."""
+
+    result = []
+
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == k:
+                result.append([arr[i], arr[j]])
+    return result
