81163eb20657d44ca7bc4d0703a4cd99b3196d5e
caoglish <caoglish.sina@gmail.com>
2014-02-03 13:46:59 +1100

    change javascript solution for binary-search-tree-check.js by using In-Order Traversal method.

--- solutions/javascript/binary-search-tree-check.js
+++ solutions/javascript/binary-search-tree-check.js
@@ -1,31 +1,27 @@
-module.exports = function (bst) {
-  // Return early if the tree does not exist.
-  if (!bst || bst.value == null) {
+//METHOD 4(Using In-Order Traversal) 
+//reference: http://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
+module.exports = function(bst) {
+  if (!bst || bst.value === null) {
     return false;
   }
 
-  // Keep track of encountered nodes.
-  var nodes = {};
+  var prev;
 
-  return (function isBST (bst) {
-    if (bst.value == null || nodes[bst.value]) {
-      return false;
+  return (function isBst(root) {
+    if ( !! root) {
+      if (!isBst(root.left)) {
+        return false;
+      }
+
+      if (prev && prev.value >= root.value) {
+        return false;
+      }
+
+      prev = root;
+
+      return isBst(root.right);
     }
 
-    // Add the current value to the nodes object.
-    nodes[bst.value] = true;
-
-    // The left value should be smaller than the current value.
-    if (bst.left && bst.left.value > bst.value) {
-      return false;
-    }
-
-    // The right value should be larger than the current value.
-    if (bst.right && bst.right.value < bst.value) {
-      return false;
-    }
-
-    // Check if we have each side and that each side is valid.
-    return (!bst.left || isBST(bst.left)) && (!bst.right || isBST(bst.right));
+    return true;
   })(bst);
 };
