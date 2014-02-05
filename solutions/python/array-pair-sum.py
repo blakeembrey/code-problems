0908b64f0eb5343f4234d5b4573e4c0a936b7a0a
Nurazem Kaldybaev <nurazemkg@gmail.com>
2014-02-05 13:13:04 +0400

    Add two more solutions with different complexities + Unit tests

--- solutions/python/array-pair-sum.py
+++ solutions/python/array-pair-sum.py
@@ -1,7 +1,12 @@
+import unittest
+
 """solution to the array pair sum problem"""
 
-def pair_sum_arrays(k, arr):
-    """returns the array of pairs using an iterative method."""
+def array_pair_sum_iterative(arr, k):
+    """
+    returns the array of pairs using an iterative method.
+    complexity: O(n^2)
+    """
 
     result = []
 
@@ -11,3 +16,68 @@ def pair_sum_arrays(k, arr):
                 result.append([arr[i], arr[j]])
 
     return result
+
+
+def array_pair_sum_sort(arr, k):
+    """
+    first sort the array and then use binary search to find pairs.
+    complexity: O(nlogn)
+    """
+
+    result = []
+    arr.sort()
+
+    for i in range(len(arr)):
+        if k - arr[i] in arr[i+1:]:
+            result.append([arr[i], k - arr[i]])
+
+    return result
+
+
+def array_pair_sum_hash_table(arr, k):
+    """
+    Use a hash table to store array elements of pairs.
+    complexity: O(n)
+    """
+
+    result = []
+    hash_table = {}
+
+    for e in arr:
+        if e in hash_table:
+            result.append([k - e, e])
+        else:
+            hash_table[k - e] = True
+    result.reverse()
+
+    return result
+
+
+# Unit tests
+class array_pair_sum_tests(unittest.TestCase):
+
+    def setUp(self):
+        self.arr1 = [3, 4, 5, 6, 7]
+        self.arr2 = [3, 4, 5, 4, 4]
+        self.result1 = [[3, 7], [4, 6]]
+        self.result2 = [[3, 5], [4, 4], [4, 4], [4, 4]]
+
+    def test_one(self):
+        self.assertEqual(
+            array_pair_sum_iterative(self.arr1, 10), self.result1)
+        self.assertEqual(
+            array_pair_sum_sort(self.arr1, 10), self.result1)
+        self.assertEqual(
+            array_pair_sum_hash_table(self.arr1, 10), self.result1)
+
+    def test_two(self):
+        self.assertEqual(
+            array_pair_sum_iterative(self.arr2, 8), self.result2)
+        self.assertEqual(
+            array_pair_sum_sort(self.arr2, 8), self.result2)
+        self.assertEqual(
+            array_pair_sum_hash_table(self.arr2, 8), self.result2)
+
+
+if __name__ == '__main__':
+    unittest.main()
