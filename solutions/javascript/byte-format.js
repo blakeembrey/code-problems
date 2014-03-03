2c8a030d03a6eedc5c04f851d836108d84387c9a
caoglish <caoglish.sina@gmail.com>
2014-03-04 10:30:51 +1100

    1. add unit test 2. fix byte-format a bug:if number larger than 1024 YB, the result will be like xxxxx.xx undefined

--- solutions/javascript/byte-format.js
+++ solutions/javascript/byte-format.js
@@ -3,10 +3,11 @@ module.exports = function (value, precision) {
   var factor   = Math.pow(10, precision > 0 ? precision : 2);
   var suffix   = 0;
 
-  while (value >= 1024 && suffixes[++suffix]) {
+  while (value >= 1024 && suffixes[++suffix]&&suffix<suffixes.length) {
     value /= 1024;
   }
 
   // Return the number rounded to precision.
-  return (Math.round(value * factor) / factor) + ' ' + suffixes[suffix];
+  return (Math.round(value * factor) / factor) + ' ' +
+    (suffix<suffixes.length?suffixes[suffix]:suffixes[suffixes.length-1]);
 };
---
+++ tests/javascript/byte-format.js
@@ -0,0 +1,51 @@
+var expect = require('chai').expect;
+var byteFormat = require('../../solutions/javascript/byte-format');
+
+describe('Byte format', function() {
+	it('should show B format', function() {
+		expect(byteFormat(1022)).to.equal('1022 B');
+	});
+
+	it('should show KB format', function() {
+		expect(byteFormat(10221)).to.equal('9.98 KB');
+	});
+
+	it('should show KB format rounded to 3 digital', function() {
+		expect(byteFormat(10221, 3)).to.equal('9.981 KB');
+	});
+
+	it('should show MB format', function() {
+		expect(byteFormat(1022932324)).to.equal('975.54 MB');
+	});
+
+	it('should show GB format', function() {
+		expect(byteFormat(1022932123237)).to.equal('952.68 GB');
+	});
+
+	it('should show TB format', function() {
+		expect(byteFormat(1022932453333234)).to.equal('930.35 TB');
+	});
+
+	it('should show PB format', function() {
+		expect(byteFormat(1022932453333234444)).to.equal('908.55 PB');
+	});
+
+	it('should show EB format', function() {
+		expect(byteFormat(1022932453333234444324)).to.equal('887.25 EB');
+	});
+
+	it('should show ZB format', function() {
+		expect(byteFormat(1022932453333234444324454)).to.equal('866.46 ZB');
+	});
+
+	it('should show YB format', function() {
+		expect(byteFormat(10243245333323444432445431)).to.equal('8.47 YB');
+	});
+
+	it('should show YB format when number larger than 1024YB', function() {
+		expect(byteFormat(232932453333234444324454333424324)).to.equal('192677209.43 YB');
+	});
+
+
+
+});
