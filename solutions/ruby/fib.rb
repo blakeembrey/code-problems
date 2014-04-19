a23f6c9b06f1be3ba14883c76a4140f37de5a4ed
Gabriel Ruiz <gruiz17@berkeley.edu>
2014-04-18 17:57:03 -0700

    added iterative approach to fibonacci in fib.rb

--- solutions/ruby/fib.rb
+++ solutions/ruby/fib.rb
@@ -1,5 +1,20 @@
+# this version has exponential runtime, O(2^n)
 def fib(n)
   return 0 if n == 0
   return 1 if n == 1 || n == 2
   fib(n - 2) + fib(n - 1)
 end
+
+# this version is more efficient, with linear runtime, O(n)
+def fib(n)
+  return 0 if n == 0
+  return 1 if n == 1
+  prev, curr, i = 0, 1, 1
+  while i < n
+    tmp = curr
+    curr += prev
+    prev = tmp
+    i += 1
+  end
+end
+  
