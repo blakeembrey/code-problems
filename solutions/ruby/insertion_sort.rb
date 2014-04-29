3a21c42cb54bd92eb867f75dafe3aaf3e8a9ea76
gruiz17 <gruiz17@berkeley.edu>
2014-04-29 16:13:33 -0700

    whoops

--- solutions/ruby/insertion_sort.rb
+++ solutions/ruby/insertion_sort.rb
@@ -1,10 +1,12 @@
 def insertion_sort(arr)
   1.upto(arr.length-1) do |i|
     j = i
-    while j != 0 || arr[j] < arr[j-1]
-      tmp = arr[j-1]
-      arr[j-1] = arr[j]
-      arr[j] = tmp
+    while j != 0
+      if arr[j] < arr[j-1]
+        tmp = arr[j-1]
+        arr[j-1] = arr[j]
+        arr[j] = tmp
+      end
       j -= 1
     end
   end
