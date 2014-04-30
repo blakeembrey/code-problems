de530961140140d2b1b2940e43faa4b22ae41958
megawac <megawac@gmail.com>
2014-04-30 18:59:53 -0400

    Fix bug in largest-continous-sequence.py

--- solutions/python/largest-continuous-sum.py
+++ solutions/python/largest-continuous-sum.py
@@ -1,17 +1,30 @@
+import unittest
 """solution to the largest-continuous-sum problem"""
 
 def largest_continuous_sum(arr):
     """returns the highest sum of a continuous sequence in a given list"""
-
-    largest = 0
-    queue = []
+    largest = sum = 0
+    last = False
     for num in arr:
-        if len(queue) > 0 and queue[-1] + 1 != num:
-            sum = reduce(lambda x, y: x + y, queue)
-            if largest < sum:
-                largest = sum
-            queue = []
-
-        queue.append(num)
-
+        if last and last + 1 == num:
+            sum += num
+        else:
+            sum = num
+        if largest < sum:
+            largest = sum
+        last = num
     return largest
+
+class LargestContinousSequenceTest(unittest.TestCase):
+    def test_largest_continuous_sum(self):
+        sum = largest_continuous_sum([1, 2, 3, 4])
+        self.assertEqual(sum, 10)
+
+        sum = largest_continuous_sum([1, 2, 3, 1, 2, 3, 4])
+        self.assertEqual(sum, 10)
+
+        sum = largest_continuous_sum([1, 2, 3, 2, 5, 4])
+        self.assertEqual(sum, 6)
+
+if __name__ == '__main__':
+    unittest.main()
