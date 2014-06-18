c23567838470720740a5961d6242eae91df1ffbc
matt <matt@clover.com>
2014-06-17 22:09:01 -0700

    more concise javascript solution for first-non-repeated-character

--- solutions/javascript/first-non-repeated-character.js
+++ solutions/javascript/first-non-repeated-character.js
@@ -1,26 +1,14 @@
 module.exports = function (string) {
-  var checkChar,
-      prevCharacter;
+  var i,
+      matchLeft,
+      matchRight;
 
-  checkChar = (function () {
-    var repeated = false;
+  for (i = 0; i < string.length; i++) {
+    matchLeft = (i - 1 >= 0 && string[i] === string[i - 1]);
+    matchRight = (i + 1 < string.length && string[i] === string[i + 1]);
 
-    return function (char) {
-      if (prevCharacter && char === prevCharacter) {
-        repeated = true;
-      }
-      if (prevCharacter && char !== prevCharacter) {
-        if (!repeated) { return true; }
-        repeated = false;
-      }
-      prevCharacter = char;
-      // Return false to say it's not been repeated
-      return false;
-    };
-  })();
-
-  // Interate one extra time past the last character
-  for (var i = 0; i <= string.length; i++) {
-    if (checkChar(string[i])) { return prevCharacter; }
+    if (!matchLeft && !matchRight) {
+      return string[i];
+    }
   }
 };
