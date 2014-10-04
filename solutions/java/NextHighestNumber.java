d0775e1651ebac7016fa7908b7508fdd51320014
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-04 16:27:54 -0400

    Next highest number Java

---
+++ solutions/java/NextHighestNumber.java
@@ -0,0 +1,25 @@
+public class NextHighestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NextHighestNumber nhn = new NextHighestNumber();
+		Integer next = nhn.getNextHighestNumber(32233);
+		if (next != null)
+			System.out.println(next);
+	}
+
+	public Integer getNextHighestNumber(int n) {
+		String sNumber = String.valueOf(n);
+		char[] nums = sNumber.toCharArray();
+		for (int i = nums.length - 1; i > 0; i--) {
+			for (int j = i - 1; j >= 0; j--)
+				if (Integer.valueOf(nums[i]) > Integer.valueOf(nums[j])) {
+					char t = nums[i];
+					nums[i] = nums[j];
+					nums[j] = t;
+					return Integer.valueOf(new String(nums));
+				}
+		}
+		return null;
+	}
+}
