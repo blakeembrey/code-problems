aeb7106bc7d33479b9eb2017ff926791fcfe27cf
Julich Mera <mera7617@gmail.com>
2014-11-11 22:37:39 -0500

    Add ruby solution for balanced brackets problem, fix minor grammar error in CONTRIBUTING.md

---
+++ solutions/ruby/balanced_brackets.rb
@@ -0,0 +1,10 @@
+def balanced_brackets(str)
+  brackets = { '(' => ')', '{' => '}', '[' => ']' }
+
+  arr = []
+  str.each_char do |c|
+    if brackets[c] then arr << c 
+    else return false if arr.size < 1 || (brackets.key(c) != arr.pop) end
+  end
+  arr.size == 0 ? true : false
+end
