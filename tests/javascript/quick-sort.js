8fed5657e4a4036486fb4eda5665a595bd2ce512
Adam Loving <adamloving@gmail.com>
2014-12-29 16:04:12 -0800

    add tests for javascript quick-sort

--- tests/javascript/quick-sort.js
+++ tests/javascript/quick-sort.js
@@ -4,10 +4,21 @@ var assert = require('assert');
 var quickSort = require('../../solutions/javascript/quick-sort');
 
 describe('Quick Sort', function(){
-  var unsorted = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-  var sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  it('handles empty list', function() {
+    var results = quickSort([]);
+    assert(Array.isArray(results), 'Expected result to be array');
+    assert(results.length === 0, 'Expected array to be empty');
+  });
+
+  it('handles list of one', function() {
+    var results = quickSort([1]);
+    assert.deepEqual(results, [1], 'Expected arrays to match');
+  });
 
   it('sorts an array of numbers', function() {
+    var unsorted = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+    var sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     var results = quickSort(unsorted);
     assert.deepEqual(results, sorted, 'Expected arrays to match');
   });
@@ -19,5 +30,12 @@ describe('Quick Sort', function(){
 
     assert.deepEqual(results, sorted, 'Expected arrays to match');
   });
+
+  it('leaves input array intact', function() {
+    var words = ['apple','beta','carrot'];
+    quickSort(words);
+    assert(words.length === 3);
+  });
+
 
 });
