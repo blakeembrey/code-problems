1aaa8656b1b45fe69e7bf4cdfebc6360f21c978e
caoglish <caoglish.sina@gmail.com>
2015-01-23 09:54:55 +1100

    improve the byte-format.js: using math instead of loop.

--- solutions/javascript/byte-format.js
+++ solutions/javascript/byte-format.js
@@ -1,16 +1,9 @@
 module.exports = function (value, precision) {
   var suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   var factor   = Math.pow(10, precision > 0 ? precision : 2);
-  var suffix   = 0;
-
-  while (value >= 1024 && suffixes[++suffix] && suffix < suffixes.length) {
-    value /= 1024;
-  }
-
-  if (suffix >= suffixes.length) {
-    suffix = suffixes.length - 1;
-  }
-
-  // Return the number rounded to precision.
-  return (Math.round(value * factor) / factor) + ' ' + suffixes[suffix];
+  var suffix=Math.min (Math.floor(Math.log(value)/Math.log(1024)),
+    suffixes.length-1);
+  return (Math.round(value/Math.pow(1024,suffix) * factor)  / factor) +
+  ' ' +
+  suffixes[suffix];
 };
