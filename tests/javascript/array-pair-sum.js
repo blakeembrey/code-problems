13e49295b6d4d4536ae21510cdd248ad3c4d2465
Blake Embrey <hello@blakeembrey.com>
2015-08-11 14:12:18 -0700

    Correct array pair sum

--- tests/javascript/array-pair-sum.js
+++ tests/javascript/array-pair-sum.js
@@ -3,10 +3,14 @@ var arrayPairSum = require('../../solutions/javascript/array-pair-sum');
 
 describe('Array Pair Sum', function () {
   it('should find pairs that equal the expected sum', function () {
-    expect(arrayPairSum(10, [3, 4, 5, 6, 7])).to.eql([[3, 7], [4, 6]]);
+    expect(arrayPairSum(10, [3, 4, 5, 6, 7])).to.eql([[4, 6], [3, 7]]);
   });
 
-  it('should allow for duplicate results', function () {
-    expect(arrayPairSum(8, [3, 4, 5, 4, 4])).to.eql([[3, 5], [4, 4], [4, 4], [4, 4]]);
+  it('should not output duplicate results', function () {
+    expect(arrayPairSum(8, [3, 4, 5, 4, 4])).to.eql([[3, 5], [4, 4]]);
+  });
+
+  it('should only allow distinct elements', function () {
+    expect(arrayPairSum(4, [1, 1, 2, 3, 4])).to.eql([[1, 3]]);
   });
 });
