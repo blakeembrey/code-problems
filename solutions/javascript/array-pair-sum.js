98265a20919a305041cf2964cf49f7891559eca5
Blake Embrey <hello@blakeembrey.com>
2015-09-26 22:32:53 -0700

    Correct `array-pair-sum` solution in JavaScript

--- solutions/javascript/array-pair-sum.js
+++ solutions/javascript/array-pair-sum.js
@@ -1,21 +1,19 @@
-module.exports = function (k, array) {
+module.exports = function arraypairsum (k, array) {
   var hash = {};
   var pairs = [];
 
-  // Loop through the array once, storing the results in an object for a
-  // time complexity of O(n) - the naive solution consists of two for loops
-  // which results in a complexity of O(n^2)
+  // Iterate over the array, tracking the times each number appears. For each
+  // new number, we calculate the difference to `k` and look up the number of
+  // times that number has been seen and push those occurances in pairs output.
   array.forEach(function (number) {
-    var target = k - number;
+    var diff = k - number;
+    var len = hash[diff];
 
-    // Make sure the value in unused and it's a unique pair
-    if (hash[target] === 1 && number + target === k) {
-      pairs.push([target, number]);
-    } else {
-      hash[number] = 0;
+    while (len--) {
+      pairs.push([number, diff]);
     }
 
-    hash[number]++;
+    hash[number] = (hash[number] + 1) || 1;
   });
 
   return pairs;
