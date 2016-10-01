968467c3cab91b590ef940d4566f6ca64345f6ce
Blake Embrey <hello@blakeembrey.com>
2016-09-30 17:55:52 -0700

    Improve the palindrome solution (way better now!)

--- solutions/javascript/largest-palindrome.js
+++ solutions/javascript/largest-palindrome.js
@@ -1,24 +1,29 @@
-module.exports = function (str) {
-  var palindromes = [];
+function isPalindrome (str) {
+  const mid = Math.floor(str.length / 2)
 
-  var walkPalindrome = function (result, str, leftIndex, rightIndex) {
-    while (str[leftIndex - 1] === str[rightIndex + 1]) {
-      result = str[leftIndex -= 1] + result + str[rightIndex += 1];
-    }
-    palindromes.push(result);
-  };
-
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] === str[i - 1]) {
-      walkPalindrome(str[i] + str[i - 1], str, i - 1, i);
-    } else if (str[i] === str[i + 1]) {
-      walkPalindrome(str[i] + str[i + 1], str, i, i + 1);
-    } else if (str[i - 1] === str[i + 1]) {
-      walkPalindrome(str[i - 1] + str[i] + str[i + 1], str, i - 1, i + 1);
+  for (let i = 0; i < mid; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false
     }
   }
 
-  return palindromes.reduce(function (memo, str) {
-    return str.length > memo.length ? str : memo;
-  }, '');
-};
+  return true
+}
+
+module.exports = function longestPalindrome (str) {
+  let longest = ''
+
+  for (let i = 0; i < str.length; i++) {
+    let len = str.length
+
+    while (len > i && len - i > longest.length) {
+      const substring = str.substring(i, len--)
+
+      if (isPalindrome(substring)) {
+        longest = substring
+      }
+    }
+  }
+
+  return longest
+}
