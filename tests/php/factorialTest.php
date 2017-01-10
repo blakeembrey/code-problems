0570b7bde62b49e2a4c5ed64510c0e828a477077
Caoglish <caoglish.sina@gmail.com>
2017-01-10 13:45:47 +1100

    Add PHPUnit test suite (#176)

---
+++ tests/php/factorialTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once  'solutions/php/factorial.php';
+
+class StackTest extends TestCase
+{
+    public function test_factorial()
+    {
+        $this->assertEquals(1,factorial(1) );
+        $this->assertEquals(2,factorial(2) );
+        $this->assertEquals(6,factorial(3) );
+        $this->assertEquals(120,factorial(5) );
+        $this->assertEquals(2432902008176640000,factorial(20) );
+    }
+}
+?>
