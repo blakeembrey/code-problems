6acf7b126ff90a976856ce33b0dd9d9009219fc7
caoglish <yingli.cao@rmit.edu.au>
2017-01-11 17:47:33 +1100

    - add factorialReverse and factorialRecursive test

--- tests/php/factorialTest.php
+++ tests/php/factorialTest.php
@@ -13,4 +13,24 @@ class FactorialTest extends TestCase
         $this->assertEquals(120, factorial(5));
         $this->assertEquals(2432902008176640000, factorial(20));
     }
+
+    public function test_factorialReverse()
+    {
+        $this->assertEquals(1, factorialReverse(1));
+        $this->assertEquals(2, factorialReverse(2));
+        $this->assertEquals(6, factorialReverse(3));
+        $this->assertEquals(120, factorialReverse(5));
+        $this->assertEquals(2432902008176640000, factorialReverse(20));
+    }
+
+    public function test_factorialRecursive()
+    {
+        $this->assertEquals(1, factorialRecursive(1));
+        $this->assertEquals(2, factorialRecursive(2));
+        $this->assertEquals(6, factorialRecursive(3));
+        $this->assertEquals(120, factorialRecursive(5));
+        $this->assertEquals(2432902008176640000, factorialRecursive(20));
+    }
+
+   
 }
