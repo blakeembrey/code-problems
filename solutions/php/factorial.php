a448e31440d7c4a307a7c01677d325693887d7a1
Caoglish <caoglish.sina@gmail.com>
2017-01-12 11:40:58 +1100

    Update some phpunit test cases (#178)

--- solutions/php/factorial.php
+++ solutions/php/factorial.php
@@ -1,35 +1,36 @@
 <?php
 
-  // Iterative solution.
-  function factorial ($number) {
+// Iterative solution.
+function factorial($number)
+{
     $result = 1;
 
     for ($i = 1; $i <= $number; $i++) {
-      $result *= $i;
+        $result *= $i;
     }
 
     return $result;
-  }
+}
 
-  // Iterative using a reverse loop.
-  function factorialReverse ($number) {
+// Iterative using a reverse loop.
+function factorialReverse($number)
+{
     $result = 1;
 
     while ($number != 1) {
-      $result *= $number;
-      $number--;
+        $result *= $number;
+        $number--;
     }
 
     return $result;
-  }
+}
 
-  // Recursive solution.
-  function factorialRecursive ($number) {
+// Recursive solution.
+function factorialRecursive($number)
+{
     if ($number == 1) {
-      return 1;
+        return 1;
     }
 
     return $number * factorialRecursive($number - 1);
-  }
-
-?>
+}
--- tests/php/factorialTest.php
+++ tests/php/factorialTest.php
@@ -7,10 +7,31 @@ class FactorialTest extends TestCase
 {
     public function test_factorial()
     {
-        $this->assertEquals(1, factorial(1));
-        $this->assertEquals(2, factorial(2));
-        $this->assertEquals(6, factorial(3));
-        $this->assertEquals(120, factorial(5));
-        $this->assertEquals(2432902008176640000, factorial(20));
+        $this->verifyFactorial("factorial");
+    }
+
+    public function test_factorialReverse()
+    {
+        $this->verifyFactorial("factorialReverse");
+    }
+
+    public function test_factorialRecursive()
+    {
+        $this->verifyFactorial("factorialRecursive");
+    }
+
+    private function verifyFactorial($fn)
+    {
+        $testDataMap = [
+            ["input" => 1, "expect" => 1],
+            ["input" => 2, "expect" => 2],
+            ["input" => 3, "expect" => 6],
+            ["input" => 5, "expect" => 120],
+            ["input" => 20, "expect" => 2432902008176640000],
+        ];
+
+        foreach ($testDataMap as $data) {
+            $this->assertEquals($data["expect"], call_user_func($fn, $data["input"]));
+        }
     }
 }
